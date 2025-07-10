856c503da4a0dbe808d072d86f02d2045100fbbd
ArthurWalker <whitefang17996@gmail.com>
2025-07-10 23:45:30 +0100

    Basic FastAPI

--- Exercise1/main.py
+++ Exercise1/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    text:str
+    is_done:bool = False
 
 items = []
 
@@ -9,11 +14,17 @@ def root():
     return {"message": "Hello, World!"}
 
 @app.post("/items")
-def create_item(item:str):
+def create_item(item:Item):
     items.append(item)
     return {"message": "Item created successfully", "item": items}
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", response_model= Item)
 def get_item(item_id:int):
-    item = items[item_id]
-    return {"item": item}
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code = 404, detail = "Item not found")
+    
+@app.get("/items",response_model = list[Item])
+def list_items(limit:int=3):
+    return items[:limit]
